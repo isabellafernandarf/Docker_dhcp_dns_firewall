ea0c84a1382ad4aef553ce3456d9e59afa22d99e
Isabela Fernanda <110735290+isabellafernandarf@users.noreply.github.com>
2024-03-25 22:07:13 -0300

    Update criacao_da_rede.sh

--- criacao_da_rede.sh
+++ criacao_da_rede.sh
@@ -1,11 +1,11 @@
-sudo su
+docker network create --driver bridge minha_rede --subnet=192.168.1.0/24
 
 docker build -t ubuntu-dhcp -f Dockerfile .
 docker build -t ubuntu-dns -f Dockerfile.dns .
 docker build -t ubuntu-firewall -f Dockerfile.firewall .
 docker build -t ubuntu-cliente -f Dockerfile.cliente .
 
-docker run -d --network rede ubuntu-dhcp
-docker run -d --network rede ubuntu-dns
-docker run -d --network rede ubuntu-firewall
-docker run -d --network rede ubuntu-cliente tail -f /dev/null
+docker run -d --network minha_rede --cap-add=NET_ADMIN ubuntu-dhcp
+docker run -d --network minha_rede ubuntu-dns
+docker run -d --network minha_rede --privileged ubuntu-firewall
+docker run -d --network minha_rede ubuntu-cliente tail -f /dev/null
